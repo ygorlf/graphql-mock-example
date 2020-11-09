30d6c91b1d983f3e833b9bacbf81fbbd28236e41
Ygor Fonseca <ygor.luiz.fonseca@gmail.com>
2020-11-08 23:53:28 -0300

    Mocking GraphQL Request

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import {
-  ApolloClient,
-  InMemoryCache,
-  gql,
-  useQuery,
-} from '@apollo/client';
+  GraphQLClient,
+  gql
+} from 'graphql-request'
 
 import { mockedServer } from './server/';
 
 // initialize a GraphQL client
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  uri: `${process.env.REACT_APP_API}`
+const client = new GraphQLClient(`${process.env.REACT_APP_API}`, {
+  headers: {},
 });
 
 // write a GraphQL query that asks for names and codes for all countries
@@ -31,22 +28,44 @@ if (process.env.NODE_ENV === 'development') {
 
 // create a component that renders a select input for coutries
 function CountrySelect() {
+  const [loading, setLoading] = useState(true);
   const [country, setCountry] = useState('US');
-  const {data, loading, error} = useQuery(LIST_COUNTRIES, {client});
+  const [countries, setCountries] = useState([]);
 
-  if (loading || error) {
-    return <p>{error ? error.message : 'Loading...'}</p>;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await client.request(
+          LIST_COUNTRIES,
+          {}
+        );
+
+        setCountries(response.countries);
+        setLoading(false);
+      } catch (err) {
+        console.error(err);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [])
+
+  if (loading) {
+    return <p>Loading...</p>;
   }
 
   return (
     <select value={country} onChange={event => setCountry(event.target.value)}>
-      {data.countries.map(country => (
+      {countries.map(country => (
         <option key={country.code} value={country.code}>
           {country.name}
         </option>
       ))}
     </select>
   );
+
+  
 }
 
 ReactDOM.render(<CountrySelect />, document.getElementById('root'));
