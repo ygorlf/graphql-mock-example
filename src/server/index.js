7f950fb815af4ea1ffbe87b67a3c6de0d7ea4dd1
Ygor Fonseca <ygor.luiz.fonseca@gmail.com>
2020-11-08 23:55:04 -0300

    Updating Mirage Config & Data

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,20 @@
 // Libs
 import { createServer, Model } from 'miragejs';
+import { buildSchema, graphql } from 'graphql';
 
 // Data
 import { countries } from './data/countries';
+
+const graphqlSchema = buildSchema(`
+  type Query {
+    countries: [Country]
+  }
+
+  type Country {
+    name: String
+    code: String
+  }
+`);
 
 export const mockedServer = (environment) => createServer({
   models: {
@@ -11,7 +23,7 @@ export const mockedServer = (environment) => createServer({
   seeds(server) {
     for (let i = 0; i < countries.length; i++) {
       const element = countries[i];
-      
+
       server.create(
         'country',
         {
@@ -21,10 +33,18 @@ export const mockedServer = (environment) => createServer({
     }
   },
   routes() {
-    this.post(`${process.env.REACT_APP_API}`, (schema) => {
-      return {
-        books: schema.countries.all().models
+    this.post(`${process.env.REACT_APP_API}`, (schema, request) => {
+      let requestJson = JSON.parse(request.requestBody);
+      let query = requestJson.query;
+      let variables = requestJson.variables;
+
+      let resolver = {
+        countries() {
+          return schema.db.countries;
+        }
       };
+
+      return graphql(graphqlSchema, query, resolver, null, variables);
     });
   },
 });
